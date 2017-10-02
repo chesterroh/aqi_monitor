be279a62a8e2277e881fb23e5814958323fd6bd4
Chester Jungseok Roh <chester.roh@gmail.com>
2017-10-02 17:04:59 +0900

    unnecessary data deleted

--- thread_client.py
+++ thread_client.py
@@ -6,14 +6,10 @@ import urllib.parse
 import time
 
 aqi_request = {}
-
-aqi_request['temp']=100
-aqi_request['humid']=78.0
 aqi_request['aqi01']=81
 aqi_request['aqi25']=88
 aqi_request['aqi10']=100
 aqi_request['voc']=11
-aqi_request['co2']=33
 
 def log_aqi(delay,run_event):
     while run_event.is_set():
@@ -22,7 +18,7 @@ def log_aqi(delay,run_event):
 
 def log_server_http():
     host = "http://localhost:8000"
-    uri = "/polls/logaqi?temp=%s&humid=%s&aqi01=%s&aqi25=%s&aqi10=%s&voc=%s&co2=%s" % ( aqi_request['temp'], aqi_request['humid'], aqi_request['aqi01'], aqi_request['aqi25'], aqi_request['aqi10'], aqi_request['voc'], aqi_request['co2'] )
+    uri = "/polls/logaqi?aqi01=%s&aqi25=%s&aqi10=%s&voc=%s" % ( aqi_request['aqi01'], aqi_request['aqi25'], aqi_request['aqi10'], aqi_request['voc'] )
     
     f = urllib.request.urlopen(host+uri)
     print(f.read().decode('utf-8'))
@@ -39,7 +35,7 @@ def main():
 
     try:
         while 1:
-            aqi_request['temp'] += 1
+            aqi_request['aqi01'] += 1
             time.sleep(1)
     except KeyboardInterrupt:
         print("keyboard interrupt caught")
@@ -50,3 +46,4 @@ def main():
 if __name__ == "__main__":
     main()
     
+
